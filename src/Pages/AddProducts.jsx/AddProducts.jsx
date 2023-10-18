9eae4f74c7a5b33069a9d06426859109d9aa6d9b
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2023-10-18 06:32:37 +0600

    products post method is done

--- src/Pages/AddProducts.jsx/AddProducts.jsx
+++ src/Pages/AddProducts.jsx/AddProducts.jsx
@@ -1,12 +1,55 @@
 import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 const AddProducts = () => {
-    const [changeValue , setChangeValue] = useState("")
-    // handleBrandValue
-    const handleBrandValue =e=>{
-        const newValue = e.target.value 
-        console.log(newValue);
+  const [brandName, setBrandName] = useState("");
+  // handleBrandValue
+  const handleBrandValue = (e) => {
+    const newValue = e.target.value;
+    setBrandName(newValue);
+  };
+
+  //   handleAddProducts
+  const handleAddProducts =async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const productName = e.target.productName.value;
+    const price = e.target.price.value;
+    const description = e.target.description.value;
+    const img = e.target.img.value;
+    const type = e.target.type.value;
+    const rating = e.target.rating.value;
+    if (rating.length >= 2) {
+      return toast.error("Rating dosen't up to 5");
     }
+    const stR = parseFloat(rating);
+    if (stR > 5) {
+      return toast.error("Rating dosen't more then 5");
+    }
+    if (!brandName) {
+      return toast.error("Please select your Brand name.");
+    }
+    // console.log(productName, price, description, img, type, rating, brandName);
+    const products  = {productName, price, description, img, type, rating, brandName}
+
+    try {
+        const res = await fetch("http://localhost:5000/products" , {
+            method:"POST", 
+            headers:{
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(products)
+        })
+        const data= await res.json();
+        if (data.insertedId) {
+            toast.success("Products added successfully done. ")
+            form.reset()
+        }
+    } catch (error) {
+        console.error(error);
+    }
+
+  };
   return (
     <section
       style={{
@@ -20,7 +63,7 @@ const AddProducts = () => {
             Add Your Products
           </h1>
 
-          <form>
+          <form onSubmit={handleAddProducts}>
             <div className=" grid grid-cols-1 sm:grid-cols-2 gap-7">
               <div className="form-control">
                 <label className="label">
@@ -29,7 +72,8 @@ const AddProducts = () => {
                 <label className="input-group">
                   <input
                     type="text"
-                    name="name"
+                    name="productName"
+                    required
                     placeholder="Name"
                     className="input input-bordered w-full"
                   />
@@ -40,19 +84,19 @@ const AddProducts = () => {
                   <span className="label-text text-white">Brand Name</span>
                 </label>
                 <select
-                onChange={handleBrandValue}
-                className="select select-secondary w-full max-w-xs">
+                  onChange={handleBrandValue}
+                  className="select select-secondary w-full max-w-xs"
+                >
                   <option disabled selected>
                     Select your brand name
                   </option>
                   <option value={"Intel"}>Intel</option>
                   <option value={"Samsung"}>Samsung</option>
-                  <option value={"Apple"} >Apple</option>
+                  <option value={"Apple"}>Apple</option>
                   <option value={"Sony"}>Sony</option>
                   <option value={"AMD"}>AMD</option>
                   <option value={"Microsoft"}>Microsoft</option>
                 </select>
-              
               </div>
 
               <div className="form-control">
@@ -61,7 +105,8 @@ const AddProducts = () => {
                 </label>
                 <label className="input-group">
                   <input
-                    type="text"
+                    type="number"
+                    required
                     placeholder="Price"
                     name="price"
                     className="input input-bordered w-full"
@@ -78,6 +123,7 @@ const AddProducts = () => {
                   <input
                     type="text"
                     name="description"
+                    required
                     placeholder="Description"
                     className="input input-bordered w-full"
                   />
@@ -90,6 +136,7 @@ const AddProducts = () => {
                 <label className="input-group">
                   <input
                     type="number"
+                    required
                     name="rating"
                     placeholder="Rating"
                     className="input input-bordered w-full"
@@ -104,6 +151,7 @@ const AddProducts = () => {
                   <input
                     type="text"
                     name="type"
+                    required
                     placeholder=" phone, computer etc"
                     className="input input-bordered w-full"
                   />
@@ -117,6 +165,7 @@ const AddProducts = () => {
                   <input
                     type="url"
                     name="img"
+                    required
                     placeholder="url"
                     className="input input-bordered w-full"
                   />
@@ -124,10 +173,13 @@ const AddProducts = () => {
               </div>
             </div>
 
-            <button className="btn btn-neutral w-full my-5">Add </button>
+            <button type="submit" className="btn btn-neutral w-full my-5">
+              Add{" "}
+            </button>
           </form>
         </div>
       </div>
+      <Toaster></Toaster>
     </section>
   );
 };
